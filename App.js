3c7568e62885483c4b7e21f1807331af962cc406
raizy21 <chiper_raizy@yahoo.com>
2021-12-10 01:40:39 +0200

    clean template add 1st layout

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Text style={styles.text}>search</Text>
+      </View>
+
+      <View style={styles.list}>
+        <Text style={styles.text}>list</Text>
+      </View>
+
+    </SafeAreaView>
+    <StatusBar style="auto"/>
+    </>
   );
 }
 
@@ -15,7 +24,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+
+  search :{
+    padding: 16,
+    backgroundColor: "yellow",
+  },
+
+  list: {
+    flex: 1,
+    padding:  16,
+    backgroundColor: "black",
+  },
+
+  text: {
+    color: "#fff",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+
 });
