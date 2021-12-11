02f944148e9107726788f644adf40a44e0d2261c
raizy21 <chiper_raizy@yahoo.com>
2021-12-11 18:18:17 +0200

    change to styled components

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
 import React from 'react';
 
-import { RestaurantsScreem } from './src/features/restaurants/screens/restaurants.screen';
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 
 export default function App() {
   return (
     <>
-    <RestaurantsScreem />
+    <RestaurantsScreen />
     <ExpoStatusBar />
    </>
   );
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,44 +1,37 @@
 import React from 'react';
+import styled from 'styled-components/native';
 import { Searchbar } from 'react-native-paper'; 
-import { StyleSheet, SafeAreaView, View, StatusBar, } from 'react-native';
+import { SafeAreaView, StatusBar, } from 'react-native';
 import { RestaurantInfoCard } from '../components/restaurants-info-card.components';
 
+//SafeAreaView
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${StatusBar.currentHeight}px;
+  `;
 
-export const RestaurantsScreem = () => (
+  //View Searchbar
+const SearchContainer = styled.View`
+  padding: 16px;
+  background-color: yellow;
+`;
 
-  <SafeAreaView style={styles.container}>
-  <View style={styles.search}>
+//View RestaurantInfoCard
+const RestaurantListContainer =styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: black;
+`;
+export const RestaurantsScreen = () => (
+
+  <SafeArea>
+  <SearchContainer>
     <Searchbar />
-  </View>
+  </SearchContainer>
 
-  <View style={styles.list}>
+  <RestaurantListContainer>
     <RestaurantInfoCard />
-  </View>
+  </RestaurantListContainer>
 
-</SafeAreaView>
+</SafeArea>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-
-  search :{
-    padding: 16,
-    backgroundColor: "yellow",
-  },
-
-  list: {
-    flex: 1,
-    padding:  16,
-    backgroundColor: "black",
-  },
-
-  text: {
-    color: "#fff",
-    fontSize: 32,
-    fontWeight: "bold",
-  },
-
-});
