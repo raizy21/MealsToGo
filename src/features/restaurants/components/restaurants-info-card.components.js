a8f700fc695584f76383bfd17f2fe19d265cda17
raizy21 <chiper_raizy@yahoo.com>
2021-12-13 22:50:26 +0200

    adding text component, modift spacer component, more consistency

--- src/features/restaurants/components/restaurants-info-card.components.js
+++ src/features/restaurants/components/restaurants-info-card.components.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, Image, View } from "react-native";
+import { Image } from "react-native";
 import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
 
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
+
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 
@@ -22,12 +24,6 @@ const Address = styled(Text)`
   font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
-const Title = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-
 const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
@@ -37,6 +33,7 @@ const Rating = styled.View`
   padding-top: ${(props) => props.theme.space[2]};
   padding-bottom: ${(props) => props.theme.space[2]};
 `;
+
 const Section = styled.View`
   flex-direction: row;
   align-items: center;
@@ -67,7 +64,8 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
-        <Title>{name}</Title>
+        {/* <Title>{name}</Title> */}
+        <Text variant="label">{name}</Text>
         <Section> 
           <Rating>
             {ratingArray.map(() => (
@@ -76,16 +74,16 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
           </Rating>
           <SectionEnd>
           {isClosedTemporarily && (
-              <Text variant="label" style={{ color: "red" }}>
+              <Text variant="error">
                 CLOSED TEMPORARILY
               </Text>
             )}
-            {/*      <View style={{ paddingLeft: 16 }} /> */}
-                <Spacer variant="left.large" />
-            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-            {/*      <View style={{ paddingLeft: 16 }} /> */}
-            <Spacer variant="left.large" />
-            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
           </SectionEnd>
         </Section>
         <Address>{address}</Address>
