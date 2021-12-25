85c99ce227fe0fb1400a7524b5f8f3ca0e16bc74
raizy21 <chiper_raizy@yahoo.com>
2021-12-25 12:37:40 +0200

    adding feature favourites

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import { Navigation } from './src/infrastructure/navigation';
 
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider} from "./src/services/location/location.context";
+import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -28,11 +29,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
+      <FavouritesContextProvider>
         <LocationContextProvider>
           <RestaurantsContextProvider>
             <Navigation />
           </RestaurantsContextProvider>
         </LocationContextProvider>
+        </FavouritesContextProvider>
       </ThemeProvider>
     <ExpoStatusBar />
    </>
