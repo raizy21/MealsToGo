07456ed35d56767b52307bb9ff62bf6bb7669495
raizy21 <chiper_raizy@yahoo.com>
2021-12-27 23:48:52 +0200

    adding image/cover background_ account/login/register screen

---
+++ src/features/account/screens/account.screen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { AccountBackground, AccountCover } from "../components/account.styles";
+
+export const AccountScreen = () => {
+
+  return (
+    <AccountBackground>
+      <AccountCover />
+    </AccountBackground>
+  );
+};
