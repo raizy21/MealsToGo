3d97064dd178e426a6a84900d2ac948fd4c3122e
raizy21 <chiper_raizy@yahoo.com>
2022-01-05 19:49:38 +0200

    move mock data to geocode.mock

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,3 +1,9 @@
+const { locations: locationsMock } = require("./geocode.mock");
+const url = require("url");
+
 module.exports.geocodeRequest = (request, response) => {
-  response.send('Geocode request');
-}
+  const { city } = url.parse(request.url, true).query;
+  const locationMock = locationsMock[city.toLowerCase()];
+
+  response.json(locationMock);
+};
