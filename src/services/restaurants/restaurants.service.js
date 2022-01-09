59fecc580cc152bff522dd389f0ece24cfe4f9e4
raizy21 <chiper_raizy@yahoo.com>
2022-01-09 18:41:12 +0200

    moving restaurants mock files

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,15 +1,6 @@
-import { mocks, mockImages } from "./mock";
 import camelize from "camelize";
 
-export const restaurantsRequest = (location) => {
-  return new Promise((resolve, reject) => {
-    const mock = mocks[location];
-    if (!mock) {
-      reject("not found");
-    }
-    resolve(mock);
-  });
-};
+export const restaurantsRequest = (location) => { };
 
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
