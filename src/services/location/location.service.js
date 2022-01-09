e139b97b6257852388befa4ed777db9c7505d39a
raizy21 <chiper_raizy@yahoo.com>
2022-01-09 16:47:34 +0200

    setting up location request

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,6 +1,13 @@
 import camelize from "camelize";
 
-export const locationRequest = (searchTerm) => {};
+export const locationRequest = (searchTerm) => {
+  return fetch(
+    `http://localhost:5001/mealstogo-df0f9/us-central1/geocode?city=${searchTerm}`
+  ).then((res) => {
+    return res.json();
+  });
+
+};
 
 export const locationTransform = (result) => {
   const formattedResponse = camelize(result);
