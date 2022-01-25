3983ae6b767cb121739b1bcbc87ac9ab8203e5d4
raizy21 <chiper_raizy@yahoo.com>
2022-01-25 03:30:04 +0200

    adding checkout ui

--- src/services/cart/cart.context.js
+++ src/services/cart/cart.context.js
@@ -10,6 +10,18 @@ export const CartContextProvider = ({ children }) => {
 
   const [cart, setCart] = useState([]);
   const [restaurant, setRestaurant] = useState(null);
+  const [sum, setSum] = useState(0);
+
+  useEffect(() => {
+    if (!cart.length) {
+      setSum(0);
+      return;
+    }
+    const newSum = cart.reduce((acc, { price }) => {
+      return (acc += price);
+    }, 0);
+    setSum(newSum);
+  }, [cart]);
 
   const add = (item, rst) => {
     if (!restaurant || restaurant.placeId !== rst.placeId) {
@@ -32,6 +44,7 @@ export const CartContextProvider = ({ children }) => {
         clearCart: clear,
         restaurant,
         cart,
+        sum,
       }}
     >
       {children}
